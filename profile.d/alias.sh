76ddd3a0481abcd45c72836937ad3d57d76b5214
David Eaves <null@crc1.net>
2022-05-13 19:34:59 -0400

    share fonts

--- profile.d/alias.sh
+++ profile.d/alias.sh
@@ -3,4 +3,4 @@ alias ls='ls --color=auto' 2>/dev/null
 alias nmap='nmap -Pn -oG -'
 alias pico=nano
 alias rot13="tr a-zA-Z n-za-mN-ZA-M"
-alias toilet="toilet --directory ${HOME}/bin/share/figlet-fonts"
+alias toilet="toilet --directory ${HOME}/.fonts/figlet"
