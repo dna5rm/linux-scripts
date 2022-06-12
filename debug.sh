85a65c4a682fcb1be96252d7e5f60afd0cdeee3e
David Eaves <null@crc1.net>
2022-06-12 03:13:48 -0400

    update

--- debug.sh
+++ debug.sh
@@ -1,35 +1,11 @@
 #!/bin/env -S bash
-# Adhoc script for creating network debug reports for troubleshooting.
+# Adhoc script for system debug and troubleshooting.
 
 # Verify who is running this script.
-#[[ ${EUID} -ne 0 ]] && {
-#     echo "$(basename ${0}): This script must be run as root!"
-#     exit 1
-#} || {
-
-    # List of functions.
-    bashFunc=(
-        "x2j"
-    )
-
-    # Load bash functions.
-    for func in ${bashFunc[@]}; do
-        [[ ! -e "$(dirname "${0}")/bashFunc/${func}.sh" ]] && {
-            echo "$(basename "${0}"): ${func} not found!"
-            exit 1
-        } || {
-            . "$(dirname "${0}")/bashFunc/${func}.sh"
-        }
-    done || exit 1
-
-    # Verify script requirements.
-    for req in /sbin/conntrack jq sudo x2j; do
-        type ${req} >/dev/null 2>&1 || {
-            echo >&2 "$(basename "${0}"): I require \"${req}\" but it's not installed. Aborting."
-            exit 1
-        }
-    done
-#}
+[[ ${EUID} -ne 0 ]] && {
+     echo "$(basename ${0}): This script must be run as root!"
+     exit 1
+}
 
 # Script Functions..
 function geoip ()
@@ -44,7 +20,7 @@ function geoip ()
 function nattable ()
 {
     # Return a formatted nat translation table
-    sudo conntrack -L -n --output xml 2> /dev/null | x2j | jq -r '.conntrack.flow[].meta | "\(.[2].id) \(.[0].layer4."@protoname") \(.[0].layer3.src) \(.[0].layer3.dst +":"+ .[0].layer4.dport) \(.[2].use)"'
+    xq -r '.conntrack.flow[].meta | "\(.[2].id) \(.[0].layer4."@protoname") \(.[0].layer3.src) \(.[0].layer3.dst +":"+ .[0].layer4.dport) \(.[2].use)"' <(conntrack -L -n --output xml 2> /dev/null)
 }
 
 ### BEGIN ###
@@ -56,32 +32,32 @@ if [[ "${1}" == "arp" ]]; then
 # IP passthrough to vtysh
 elif [[ "${1}" == "ip" ]]; then
     echo "$(basename "${0}"): show ${@}"
-    sudo vtysh -c "show ${1} ${2} ${3} ${4} ${5}"
+    vtysh -c "show ${1} ${2} ${3} ${4} ${5}"
 
 # IPsec
 elif [[ "${1}" == "ipsec" ]]; then
     echo "$(basename "${0}"): ${@}"
 
     if [[ "${2}" == "det"* ]]; then
-        sudo ipsec statusall
+        ipsec statusall
     elif [[ "${2}" == "vti" ]]; then
-        sudo ip -s xfrm state
+        ip -s xfrm state
     elif [[ "${2}" == "ike" ]]; then
-        sudo swanctl --list-sas
+        swanctl --list-sas
     elif [[ "${2}" == "log" ]]; then
-        sudo swanctl --log
+        swanctl --log
     else
-        sudo ipsec "${2:-status}"
+        ipsec "${2:-status}"
     fi
 
 # Interface statistics
 elif [[ "${1}" == "in"* ]]; then
     echo "$(basename "${0}"): show ${@}"
-    sudo vtysh -c "show ${1} ${2} ${3}"
+    vtysh -c "show ${1} ${2} ${3}"
 
 # LISTENING ports
 elif [[ "${1}" == "listen" ]]; then
-    sudo lsof -nP -iTCP -sTCP:LISTEN
+    lsof -nP -iTCP -sTCP:LISTEN
 
 # NAT Table
 elif [[ "${1}" == "nat" ]]; then
@@ -100,11 +76,11 @@ elif [[ "${1}" == "nat" ]]; then
 # LLDP: show neighbors
 elif [[ "${1}" == "lldp" ]]; then
     echo "$(basename "${0}"): lldpcli show neighbors summary"
-    sudo lldpcli show neighbors summary
+    lldpcli show neighbors summary
 
 # VLAN
 elif [[ "${1}" == "vlan" ]]; then
-    sudo awk -F'|' 'BEGIN{printf "%-16s %-7s %s\n", "Name", "ID", "Interface"}/\|.*\|/{printf "%-15s %-7s %s\n", $1, $2, $3}' /proc/net/vlan/config
+    awk -F'|' 'BEGIN{printf "%-16s %-7s %s\n", "Name", "ID", "Interface"}/\|.*\|/{printf "%-15s %-7s %s\n", $1, $2, $3}' /proc/net/vlan/config
 
 # Print Help
 else cat << EOF
