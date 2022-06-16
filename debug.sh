7b22d0acb7c846182bbd39b2014dbf60d511e694
David Eaves <null@crc1.net>
2022-06-16 09:08:11 -0400

    ipv6

--- debug.sh
+++ debug.sh
@@ -38,6 +38,34 @@ elif [[ "${1}" == "ip" ]]; then
     echo "$(basename "${0}"): show ${@}"
     vtysh -c "show ${1} ${2} ${3} ${4} ${5}"
 
+# IPv6
+elif [[ "${1}" == "ipv6" ]]; then
+
+    # show ipv6 neighbors
+    if [[ "${2}" == "nei"* ]]; then
+        printf "%-18s %-10s %-40s %-6s %-26s\n" "MAC" "State" "Address" "Dev" "Manufacturer"
+
+        ip -6 neighbor | awk '{if($(NF-1) == "router") { rtr="y"; }; if($4 == "lladdr") { print $1,$3,$5,$NF,rtr; rtr="" }}' |\
+        while read addr dev mac nud rtr; do
+
+        [[ -f "/usr/local/etc/oui.txt" ]] && {
+            oui="$(echo ${mac:0:8} | sed 's/://g;s/.*/\U&/')"
+            vendor="$(awk -F'\t' 'BEGIN{IGNORECASE = 1}/^'''$oui'''/{gsub(/\r/, ""); print $NF}' "/usr/local/etc/oui.txt")"
+        }
+
+        [[ "${rtr,,}" == "y" ]] && {
+            printf "%-18s %-10s %-40s %-6s %s\n" "${mac}" "${nud}" "${addr}" "${dev}" "${vendor:--}" |\
+            sed "1 s,.*,$(tput smul)&$(tput sgr0),"
+        } || {
+            printf "%-18s %-10s %-40s %-6s %s\n" "${mac}" "${nud}" "${addr}" "${dev}" "${vendor:--}"
+        }
+
+        done
+    else
+        echo "$(basename "${0}"): show ${@}"
+        vtysh -c "show ${1} ${2} ${3} ${4} ${5}"
+    fi
+
 # IPsec
 elif [[ "${1}" == "ipsec" ]]; then
     echo "$(basename "${0}"): ${@}"
@@ -95,6 +123,7 @@ Command:
   arp            Show arp
   dhcp           Show DHCP leases
   ip             vtysh "show ip ..." cmd pass
+  ipv6           vtysh "show ipv6 ..." cmd pass
   ipsec          Show ipsec ... [detail|ike|log|vti]
   in*            vtysh "show interface ... " cmd pass
   listen         show listening ports
