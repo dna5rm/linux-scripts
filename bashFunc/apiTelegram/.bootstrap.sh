262870c8447aeeee38210ea9a8de1cd902d85cba
David Eaves <null@crc1.net>
2023-05-08 16:44:01 -0400

    update

--- bashFunc/apiTelegram/.bootstrap.sh
+++ bashFunc/apiTelegram/.bootstrap.sh
@@ -6,11 +6,11 @@
 
 #rm -rf available-* games getting-updates inline-mode payments stickers telegram-passport updating-messages
 
-[[ ! -f "telegram-api.html" ]] && {
-    curl --silent --output "telegram-api.html" --url "https://core.telegram.org/bots/api"
+[[ ! -f "_telegram-api.html" ]] && {
+    curl --silent --output "_telegram-api.html" --url "https://core.telegram.org/bots/api"
 }
 
-awk -F'[ ><]' '/^.h/{print $2"|"$6"|"$7"|"$15,$16,$17,$18,$19,$20}' "telegram-api.html" | sed 's/ \/h.//g' | while IFS="|" read CAT NAME HREF DESC; do
+awk -F'[ ><]' '/^.h/{print $2"|"$6"|"$7"|"$15,$16,$17,$18,$19,$20}' "_telegram-api.html" | sed 's/ \/h.//g' | while IFS="|" read CAT NAME HREF DESC; do
 
     printf "%-4s %-45s %-45s %-45s\n" "${CAT}" "${NAME}" "${HREF}" "${DESC}" | sed "1 s,.*,$(tput smso)&$(tput sgr0),"
 
@@ -24,7 +24,7 @@ awk -F'[ ><]' '/^.h/{print $2"|"$6"|"$7"|"$15,$16,$17,$18,$19,$20}' "telegram-ap
         href="$(echo "${HREF}" | awk -F'"' '{print $2}')"
         api="$(echo "${DESC}" | awk '{print $1}')"
         fn="${category}/${api}.sh"
-        description="$(pandoc --from=html --to=markdown --wrap=preserve -o - <(lynx -source "telegram-api.html" | sed -n '/^<h.*'''${NAME}'''/,/^<h/p' | sed '1d;$d;s/$^$/<\/p>/;s/<a[^>]\+>/<a>/g;s/<img[^>]\+>//g') | awk -F'.' 'NR<2{print $1"."}' | sed 's/\\//g')"
+        description="$(pandoc --from=html --to=markdown --wrap=preserve -o - <(lynx -source "_telegram-api.html" | sed -n '/^<h.*'''${NAME}'''/,/^<h/p' | sed '1d;$d;s/$^$/<\/p>/;s/<a[^>]\+>/<a>/g;s/<img[^>]\+>//g') | awk -F'.' 'NR<2{print $1"."}' | sed 's/\\//g')"
 
         # Create directory if missing.
         [[ ! -d "${category}" ]] && mkdir "${category}"
@@ -51,7 +51,7 @@ awk -F'[ ><]' '/^.h/{print $2"|"$6"|"$7"|"$15,$16,$17,$18,$19,$20}' "telegram-ap
 	$(printf "\t")---
 	$(printf "\t")Telegram API Token: \\\${TELEGRAM_TOKEN} (\${TELEGRAM_TOKEN:-required})
 	$(printf "\t")---
-	$(pandoc --from=html --to=markdown -o - <(lynx -source "telegram-api.html" | sed -n '/^<h.*'''${NAME}'''/,/^<h/p' | sed '1d;$d;s/$^$/<\/p>/;s/<a[^>]\+>/<a>/g;s/<img[^>]\+>//g') | awk -F'.' '{print "\t"$0}' | sed 's/\\//g;s/`/\\`/g')
+	$(pandoc --from=html --to=markdown -o - <(lynx -source "_telegram-api.html" | sed -n '/^<h.*'''${NAME}'''/,/^<h/p' | sed '1d;$d;s/$^$/<\/p>/;s/<a[^>]\+>/<a>/g;s/<img[^>]\+>//g') | awk -F'.' '{print "\t"$0}' | sed 's/\\//g;s/`/\\`/g')
 	$(printf "\t")EOF
 	    else
 	        curl --silent --location \\
