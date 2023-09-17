21837b26f04372315f4650018debfea75a4c5594
David Eaves <null@crc1.net>
2023-09-17 15:48:04 -0400

    update

--- snmp_ifTable.sh
+++ snmp_ifTable.sh
@@ -39,7 +39,7 @@
                     ifMtu="$(sed 's/"//g' <(snmpget -Oqv -v2c -c ${community:-public} ${host} 1.3.6.1.2.1.2.2.1.4.${ifPhysAddress%% *} 2> /dev/null))"
                     ifSpeed="$(sed 's/"//g' <(snmpget -Oqv -v2c -c ${community:-public} ${host} 1.3.6.1.2.1.2.2.1.5.${ifPhysAddress%% *} 2> /dev/null))"
                     ifAlias="$(sed 's/"//g' <(snmpget -Oqv -v2c -c ${community:-public} ${host} 1.3.6.1.2.1.31.1.1.1.18.${ifPhysAddress%% *} 2> /dev/null))"
-                    printf "%-16s %-6d %-25s %-5s %-12s %-18s %s\n" "${sysName%%.*}" "${ifPhysAddress%% *}" "${ifDescr}" "${ifMtu}" "${ifSpeed}" "$(sed 's/ /:/g;s/\(.*\)/\U\1/' <<<${ifPhysAddress#* })" "${ifAlias}"
+                    printf "%-16s %-6d %-28s %-5s %-12s %-18s %s\n" "${sysName%%.*}" "${ifPhysAddress%% *}" "${ifDescr}" "${ifMtu}" "${ifSpeed}" "$(sed 's/ /:/g;s/\(.*\)/\U\1/' <<<${ifPhysAddress#* })" "${ifAlias}"
                 fi
             }
         done
