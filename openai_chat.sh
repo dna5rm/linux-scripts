b962233a000e5a2bc2e41baab50fd9f8e0874def
David Eaves <null@crc1.net>
2023-10-23 08:34:01 -0400

    update

--- openai_chat.sh
+++ openai_chat.sh
@@ -1,11 +1,11 @@
 #!/bin/env -S bash
-# Shell script to interface to the OpenAI chat models.
-# This script works by building a message list from input and interacts with the openai model to returns generated messages.
+# Shell script to interface to the OpenAI API's.
+# This script builds a message list from input and return model generated messages.
 
 # Enable for debuging
 #set -x
 
-# Verify script requirements
+# Verify script requirements.
 for req in ansible-vault curl glow tput jq yq; do
     type ${req} >/dev/null 2>&1 || {
         echo >&2 "$(basename "${0}"): I require ${req} but it's not installed. Aborting."
@@ -15,7 +15,8 @@ done && umask 0077
 
 # Read credentials from vault.
 [[ -f "${HOME}/.loginrc.vault" && "${HOME}/.vaultpw" ]] && {
-    OPENAI_API_KEY=`yq -r '.OPENAI_API_KEY' <(ansible-vault view "${HOME}/.loginrc.vault" --vault-password-file "${HOME}/.vaultpw")`
+    OPENAI_API_KEY=`yq -r '.OPENAI_API_KEY' \
+      <(ansible-vault view "${HOME}/.loginrc.vault" --vault-password-file "${HOME}/.vaultpw")`
 } || {
     echo "$(basename "${0}"): Unable to get creds from vault."
     exit 1;
@@ -47,7 +48,7 @@ function openai_chat() {
           "top_p": 1,
           "frequency_penalty": 0,
           "presence_penalty": 0,
-          "user": $user
+          "user": $user|tostring
         }')"
     }
 
@@ -70,11 +71,12 @@ function openai_chat() {
             CACHE="${HOME}/.cache/$(basename "${FUNC_SOURCE}")"
             HASH="$(echo "${user_input[@],,}" | md5sum -t | awk '{print $1}')"
 
-            chat_data=`jq -c --arg input "$(echo -e "${user_input[@]}")" '.messages += [{"role": "user", "content": $input}]' <<<${chat_data}`
+            chat_data=`jq -c --arg input "$(echo -e "${user_input[@]}")" \
+              '.messages += [{"role": "user", "content": $input}]' <<<${chat_data}`
             unset user_input
         } || { exit 0; }
 
-        # Fetch the response & cache, ignore cache if older than 30 days.
+        # Fetch the response & cache (requery if previous cache is +30 days old).
         if [[ ! -f "${CACHE}/${HASH}" ]] || [[ $(find "${CACHE}/${HASH}" -mtime +30 -print) ]]; then
             response=$(curl --silent --insecure --location --write-out "%{http_code}" \
               --request POST --url "https://api.openai.com/v1/chat/completions" \
@@ -86,11 +88,14 @@ function openai_chat() {
             case "${http_code}" in
                 400)
                     # Return the response
-                    echo "$(basename "${FUNC_SOURCE}"): $(jq -r '.error | "\(.type) - \(.message)"' <(sed '$ d' <<<${response}))"
+                    echo "$(basename "${FUNC_SOURCE}"): $(jq -r '.error | "\(.type) - \(.message)"' \
+                      <(sed '$ d' <<<${response}))"
                     exit 1;;
                 *)
                     # Return the content (fresh).
-                    chat_data=`jq -c --arg content "$(jq -r '.choices[0]|.message.content' <(sed '$ d' <<<${response}))" '.messages += [{"role": "assistant", "content": $content}]' <<<${chat_data}`
+                    chat_data=`jq -c --arg content "$(jq -r '.choices[0]|.message.content' \
+                      <(sed '$ d' <<<${response}))" '.messages += [{"role": "assistant", "content": $content}]' \
+                      <<<${chat_data}`
                     ;;
             esac
 
@@ -98,10 +103,13 @@ function openai_chat() {
             install -m 644 -D <(echo "${chat_data}") "${CACHE:-openai_chat}/${HASH}"
         else
             # Read the cached responce.
-            response="$(printf "$(sed -n '/{\"role\":\"assistant\",\"content\":\"/,/\"}],\"/ {s/.*"content":\"//;s/\"}].*,\".*//;p}' "${CACHE}/${HASH}")")"
+            response="$(printf "$(sed -n \
+              '/{\"role\":\"assistant\",\"content\":\"/,/\"}],\"/ {s/.*"content":\"//;s/\"}].*,\".*//;p}' \
+              "${CACHE}/${HASH}")")"
 
             # Return the content (cached).
-            chat_data=`jq -c --arg content "${response}" '.messages += [{"role": "assistant", "content": $content}]' <<<${chat_data}`
+            chat_data=`jq -c --arg content "${response}" \
+              '.messages += [{"role": "assistant", "content": $content}]' <<<${chat_data}`
         fi
 
         # Return the last message content.
@@ -113,30 +121,39 @@ function openai_chat() {
     }
 }
 
-# Use args or stdin as input.
-[[ -t 0 ]] && {
-    user_input="${@}"
-} || {
-    user_input=$(</dev/stdin)
-}
-
 # Entry point to script functions.
-[[ -z "${user_input}" ]] && {
+[[ "${OPENAI_API_KEY}" = "sk-"* ]] && {
 
-    # Multi-turn conversation.
+    # Use args or stdin as input.
+    [[ -t 0 ]] && {
+        user_input="${@}"
+    } || {
+        user_input=$(</dev/stdin)
+    }
+
+    # Return a model response using chat.
+    if [[ -z "${user_input}" ]]; then
+    ## Multi-turn conversation.
     cat <<-EOF | sed 's/^[ \t]*//' | glow
-	# $(basename "${0}")
-	> MODEL: **${OPENAI_MODEL:-gpt-3.5-turbo}**
-	> PROMPT: *${OPENAI_PROMPT:-You are a helpful assistant.}*
-	
-	Paste your multi-line text into the terminal, press \`CTRL+D\` when finished.
+        # $(basename "${0}")
+        > MODEL: \${OPENAI_MODEL} **(${OPENAI_MODEL:-gpt-3.5-turbo})**
+        > PROMPT: \${OPENAI_PROMPT} *(${OPENAI_PROMPT:-You are a helpful assistant.})*
+        > TEMPRATURE: \${OPENAI_TEMP} (${OPENAI_TEMP:-0.7})
+        > MAX_TOKENS: \${OPENAI_TOKENS} (${OPENAI_TOKENS:-256})
+        
+        Paste your multi-line text into the terminal, press \`CTRL+D\` when finished.
 	EOF
 
-    while true; do
+        # Loop the chat function.
+        while true; do
+            openai_chat
+        done
+
+    else
+        ## Single-turn without any conversation.
         openai_chat
-    done
+    fi
 
 } || {
-    # Single-turn without any conversation.
-    openai_chat
+    echo "$(basename "${0}"): \${OPENAI_API_KEY} is not set or invalid."
 }
