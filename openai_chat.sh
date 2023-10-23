236ee19355da77c9d72c04c632a3431c798814e1
David Eaves <null@crc1.net>
2023-10-23 18:14:31 -0400

    update

--- openai_chat.sh
+++ openai_chat.sh
@@ -34,7 +34,7 @@ function openai_chat() {
         chat_data="$(jq --null-input --arg model "${OPENAI_MODEL:-gpt-3.5-turbo}" \
             --arg prompt "${OPENAI_PROMPT:-You are a helpful assistant.}" \
             --arg temp "${OPENAI_TEMP:-0.7}" \
-            --arg tokens "${OPENAI_TOKENS:-256}" \
+            --arg tokens "${OPENAI_TOKENS:-1900}" \
             --arg user "$(whoami)" '{
           "model": $model|tostring,
           "messages": [
@@ -53,7 +53,7 @@ function openai_chat() {
     }
 
     # Validate chat_data variable is good.
-    jq -e '.' >/dev/null 2>&1 <<<${chat_data} > /dev/null && {
+    jq -e '.' >/dev/null 2>&1 <<<${chat_data:-ERR} > /dev/null && {
 
         # Read user input if no variable.
         [[ -z "${user_input}" ]] && {
@@ -69,7 +69,7 @@ function openai_chat() {
         # Append user_input to chat_data variable.
         [[ ! -z "${user_input[@]}" ]] && {
             CACHE="${HOME}/.cache/$(basename "${FUNC_SOURCE}")"
-            HASH="$(echo "${user_input[@],,}" | md5sum -t | awk '{print $1}')"
+            HASH="$(awk '{$1=$1};1' <<<${user_input[@],,} | md5sum -t | awk '{print $1}')"
 
             chat_data=`jq -c --arg input "$(echo -e "${user_input[@]}")" \
               '.messages += [{"role": "user", "content": $input}]' <<<${chat_data}`
@@ -86,30 +86,26 @@ function openai_chat() {
 
             # Handle the response or error code.
             case "${http_code}" in
-                400)
+                200)
+                    # Return the content (fresh).
+                    chat_data=`jq -c --argjson message "$(jq -r '.choices[0]|.message' \
+                      <(sed '$ d' <<<${response}))" '.messages += [$message]' <<<${chat_data}`
+                    ;;
+                *)
                     # Return the response
-                    echo "$(basename "${FUNC_SOURCE}"): $(jq -r '.error | "\(.type) - \(.message)"' \
+                    echo "$(basename "${FUNC_SOURCE}"): <${http_code}> $(jq -r '.error | "\(.type) - \(.message)"' \
                       <(sed '$ d' <<<${response}))"
                     exit 1;;
-                *)
-                    # Return the content (fresh).
-                    chat_data=`jq -c --arg content "$(jq -r '.choices[0]|.message.content' \
-                      <(sed '$ d' <<<${response}))" '.messages += [{"role": "assistant", "content": $content}]' \
-                      <<<${chat_data}`
-                    ;;
             esac
 
             # Cache the response.
             install -m 644 -D <(echo "${chat_data}") "${CACHE:-openai_chat}/${HASH}"
         else
-            # Read the cached responce.
-            response="$(printf "$(sed -n \
-              '/{\"role\":\"assistant\",\"content\":\"/,/\"}],\"/ {s/.*"content":\"//;s/\"}].*,\".*//;p}' \
-              "${CACHE}/${HASH}")")"
+            # Read the last cached responce.
+            response="$(jq -c '.messages|last' "${CACHE}/${HASH}")"
 
             # Return the content (cached).
-            chat_data=`jq -c --arg content "${response}" \
-              '.messages += [{"role": "assistant", "content": $content}]' <<<${chat_data}`
+            chat_data=`jq -c --argjson message "${response}" '.messages += [$message]' <<<${chat_data}`
         fi
 
         # Return the last message content.
@@ -119,6 +115,77 @@ function openai_chat() {
         # Something went wrong.
         echo "$(basename "${FUNC_SOURCE}"): \${chat_data} is not valid json."
     }
+}
+
+function openai_completions() {
+    # Return a completion response from provided prompt. (Legacy)
+    ## https://platform.openai.com/docs/api-reference/completions
+
+    # Variable of where this function is being called from.
+    FUNC_SOURCE="$(basename "${0}" 2> /dev/null)$([[ ! -z "${FUNCNAME[0]}" ]] && { echo "/${FUNCNAME[0]}"; })"
+
+    # Build prompt arguments from user_input.
+    [[ ! -z "${user_input}" ]] && {
+        prompt_data="$(jq --null-input --arg model "${OPENAI_MODEL:-gpt-3.5-turbo-instruct}" \
+            --arg prompt "${user_input}" \
+            --arg temp "${OPENAI_TEMP:-0.7}" \
+            --arg tokens "${OPENAI_TOKENS:-1900}" \
+            --arg user "$(whoami)" '{
+          "model": $model|tostring,
+          "prompt": $prompt|tostring,
+          "echo": false,
+          "max_tokens": $tokens|tonumber,
+          "temperature": $temp|tonumber,
+          "user": $user|tostring
+        }')"
+    }
+
+    # Create a hash of arguments for caching.
+    CACHE="${HOME}/.cache/$(basename "${FUNC_SOURCE}")"
+    HASH="$(awk '{$1=$1};1' <<<${user_input[@],,} | md5sum -t | awk '{print $1}')"
+
+    # Validate chat_data variable is good.
+    jq -e '.' >/dev/null 2>&1 <<<${prompt_data:-ERR} > /dev/null && {
+
+        # Fetch the response & cache (requery if previous cache is +30 days old).
+        if [[ ! -f "${CACHE}/${HASH}" ]] || [[ $(find "${CACHE}/${HASH}" -mtime +30 -print) ]]; then
+            response=$(curl --silent --insecure --location --write-out "%{http_code}" \
+              --request POST --url "https://api.openai.com/v1/completions" \
+              --header "Content-Type: application/json" --header "Authorization: Bearer ${OPENAI_API_KEY}" \
+              --data "${prompt_data:-{\}}")
+            http_code=$(tail -n1 <<<${response})
+
+            # Handle the response or error code.
+            case "${http_code}" in
+                200)
+                    # Add prompt used to the response.
+                    response=`jq -c --arg prompt "${user_input}" '. += {"prompt": $prompt|tostring}' <(sed '$ d' <<<${response})`
+
+                    # Cache the response (fresh).
+                    install -m 644 -D <(jq -c '.' <<<${response}) "${CACHE:openai_completions}/${HASH}"
+                    ;;
+                *)
+                    # Return the message.
+                    echo "$(basename "${FUNC_SOURCE}"): <${http_code}> $(jq -r '.error | "\(.type) - \(.message)"' \
+                      <(sed '$ d' <<<${response}))"
+                    exit 1;;
+            esac
+        else
+            # Read the last cached response.
+            response="$(awk 'END{print}' "${CACHE:openai_completions}/${HASH}")"
+        fi
+
+        # Return the first choice text.
+        jq -r '. | "# "+ .model, "", .choices[0].text' <<<${response} | glow && echo
+
+    } || {
+        # Something went wrong.
+        echo "$(basename "${FUNC_SOURCE}"): \${prompt_data} is not valid json."
+    }
+}
+
+function openai_images() {
+    echo
 }
 
 # Entry point to script functions.
@@ -139,7 +206,7 @@ function openai_chat() {
         > MODEL: \${OPENAI_MODEL} **(${OPENAI_MODEL:-gpt-3.5-turbo})**
         > PROMPT: \${OPENAI_PROMPT} *(${OPENAI_PROMPT:-You are a helpful assistant.})*
         > TEMPRATURE: \${OPENAI_TEMP} (${OPENAI_TEMP:-0.7})
-        > MAX_TOKENS: \${OPENAI_TOKENS} (${OPENAI_TOKENS:-256})
+        > MAX_TOKENS: \${OPENAI_TOKENS} (${OPENAI_TOKENS:-1900})
         
         Paste your multi-line text into the terminal, press \`CTRL+D\` when finished.
 	EOF
@@ -147,8 +214,9 @@ function openai_chat() {
         # Loop the chat function.
         while true; do openai_chat; done
     else
-        ## Single-turn without any conversation.
-        openai_chat
+        ## text_completion without any conversation.
+        #openai_chat
+        openai_completions
     fi
 
 } || {
