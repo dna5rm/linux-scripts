723a0de9413cf669ab8bf4103ae325aa9e87fa43
David Eaves <null@crc1.net>
2023-11-02 23:20:49 -0400

    update

--- bashFunc/crypt.sh
+++ bashFunc/crypt.sh
@@ -1,72 +1,65 @@
-# Use OpenSSL to encrypt/decrypt a file.
+# Use OpenSSL to encrypt/decrypt a file using a passphrase.
 
-function crypt ()
-{
-    # Verify function requirements
-    for req in openssl shred; do
-        type ${req} >/dev/null 2>&1 || {
-            # Skip basename if shell function
-            [[ "${0}" != "-bash" ]] && {
-                echo >&2 "$(basename "${0}"):${FUNCNAME[0]} - cmd/function \"${req}\" is required!"
-            } || {
-                echo >&2 "${FUNCNAME[0]} - cmd/function \"${req}\" is required!"
-           }
-            return 1
-        }
-    done
+function crypt () {
 
-    # Print reference if conditions missing
-    if [[ ${?} -eq 0 ]] && [[ -z "${1}" ]]; then
-        # Skip basename if shell function
-        [[ "${0}" != "-bash" ]] && {
-            echo -n "$(basename "${0}"):"
-        }
-        echo "${FUNCNAME[0]} - Encrypt/Decrypt a file."
-        echo "Input File: \${1} (${1:-requried})
-        Passphrase: \${2} (${2:-optional})
+    # Use args as user_input.
+    local user_input=( "${@}" )
 
-        Aborting..." | sed 's/^[ \t]*//g'
-    else
-
-        # Validate input is a file.
-        [[ -f "${1}" ]] && {
-
-            # Set passphrase to input if set.
-            [[ ! -z "${2}" ]] && {
-                passphrase="${2}"
-            # Set passphrase to user@hostname.
-            } || {
-                passphrase="$(base64 -w0 <(echo "`whoami`@`hostname -f`"))"
-            }
-
-            # Encrypt the file.
-            [[ "${1##*.}" != "enc" ]] && {
-                openssl enc -aes-256-cbc -e -md sha512 -pbkdf2 -iter 100000 -salt -in "${1}" -k "${2:-pass}" -out "${1}.enc" && {
-                    shred --force --zero --iterations 3 "${1}" && rm "${1}"
-                    printf "[crypt:%s] %s\n" "$(basename "${1}")" "Successfully encrypted!"
-                } || {
-                    rm "${1}.enc"
-                    printf "[crypt:%s] %s\n" "$(basename "${1}")" "Something went wrong!"
-                    return 1
-                }
-
-            # Decrypt the file.
-            } || {
-                openssl enc -aes-256-cbc -d -md sha512 -pbkdf2 -iter 100000 -salt -in "${1}" -k "${2:-pass}" -out "${1%%.enc}" && {
-                    shred --force --zero --iterations 3 "${1}" && rm "${1}"
-                    printf "[crypt:%s] %s\n" "$(basename "${1}")" "Successfully decrypted!"
-                } || {
-                    rm "${1%%.enc}"
-                    printf "[crypt:%s] %s\n" "$(basename "${1}")" "Something went wrong!";
-                    return 1
-                }
-            }
-
-        # Input is not a file.
-        } || {
-            printf "[crypt:%s] %s\n" "$(basename "${1}")" "Input not a file!"
-            return 1
-        }
-
+    # Run tests and set func_error any fail.
+    if ! type openssl >/dev/null 2>&1; then
+        local func_error="openssl executable not found."
+    elif ! type shred >/dev/null 2>&1; then
+        local func_error="shred executable not found."
+    elif [[ "${#user_input[@]}" == "0" ]]; then
+        local func_error="Missing files input."
     fi
+
+    # Return if any checks failed.
+    [[ ! -z "${func_error}" ]] && {
+        [[ "${0}" != -*"bash" ]] && {
+            echo >&2 "$(basename "${0}" 2> /dev/null):${FUNCNAME[0]} - ${func_error}"
+        } || {
+            echo >&2 "${FUNCNAME[0]} - ${func_error}"
+        }
+        return 1
+    } || {
+
+        # Set passphrase to element[0] otherwise default to user@host
+        if [[ ! -z "${passphrase}" ]] && [[ ! -f "${user_input[0]}" ]]; then
+            local passphrase="${user_input[0]}"
+            unset user_input[0]
+        else
+            local passphrase="$(base64 -w0 <(echo "`whoami`@`hostname -f`"))"
+        fi
+
+        # loop through all files.
+        for file in "${user_input[@]}"; do
+
+            # Validate input is a file.
+            [[ -f "${file}" ]] && {
+
+                # Encrypt the file.
+                [[ "${file##*.}" != "enc" ]] && {
+                    openssl enc -aes-256-cbc -e -md sha512 -pbkdf2 -iter 100000 -salt -in "${file}" -k "${passphrase}" -out "${file}.enc" && {
+                        shred --force --zero --iterations 3 "${file}" && rm "${file}"
+                        printf "[${FUNCNAME[0]}:%s] %s\n" "$(basename "${file}")" "Successfully encrypted!"
+                    } || { rm "${file}.enc"; return 1; }
+
+                # Decrypt the file.
+                } || {
+                    openssl enc -aes-256-cbc -d -md sha512 -pbkdf2 -iter 100000 -salt -in "${file}" -k "${passphrase}" -out "${file%%.enc}" && {
+                        shred --force --zero --iterations 3 "${file}" && rm "${file}"
+                        printf "[${FUNCNAME[0]}:%s] %s\n" "$(basename "${file}")" "Successfully decrypted!"
+                    } || { rm "${file%%.enc}"; return 1; }
+                }
+
+            } || {
+                printf "[${FUNCNAME[0]}:%s] %s\n" "$(basename "${file}")" "Input is not a file!"
+                return 1
+            }
+
+        done
+
+    }
+
 }
